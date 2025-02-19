1eada53c761c4dbea4fe3e63ac3422add35215d9
DevKeshav45 <keshavkalanke@gmail.com>
2025-02-19 21:35:59 +0530

    explore: routing, middlewares, error handling

--- src/app.js
+++ src/app.js
@@ -9,11 +9,31 @@ app.use("/hello", (req, res) => {
 });
 
 app.use("/test", (req, res) => {
+  throw new Error("Something went wrong!eeee");
   res.send("Hello, from test route");
 });
 
 app.use("/", (req, res) => {
   res.send("Hello, from Express.js backend!");
+});
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    next();
+    // res.send(err.message); 
+    // console.error(err);
+    // res.status(500).send("Something went wrong");
+  }
+});
+
+app.use("/", (err, req, res, next) => {
+  console.log(err);
+  console.log(res);
+  if (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
+    return;
+  }
 });
 
 app.listen(PORT, () => {
